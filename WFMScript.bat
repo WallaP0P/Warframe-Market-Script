c6628e0d18aca656eefc30cf888d080806258e67
WallaP0P <135544721+WallaP0P@users.noreply.github.com>
2026-07-16 02:21:53 +0200

    Delete WFMScript.bat

--- WFMScript.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-chcp 65001 >nul
-set "PATH=C:\Users\Samuele\AppData\Local\Python\bin;C:\Users\Samuele\AppData\Local\Python\pythoncore-3.14-64\Scripts;%PATH%"
-cd /d "%~dp0"
-"C:\Users\Samuele\AppData\Local\Python\bin\python.exe" mercato.py
-pause
